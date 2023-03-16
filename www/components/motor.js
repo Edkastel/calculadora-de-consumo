1248f157edd4c4a9ab37067907ff5352a077c3dc
Edkastel <edkarllosfernando2@gmail.com>
2023-03-17 04:54:57 +0900

    sim

---
+++ www/components/motor.js
@@ -0,0 +1,9 @@
+function calcularconsumo() {
+
+    km = document.getElementById('km').value
+    litros = document.getElementById('litros').value
+    
+    resultado = km / litros
+
+    document.getElementById('resultado').textContent = resultado + "km/L"
+}
